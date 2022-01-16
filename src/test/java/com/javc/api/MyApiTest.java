7829a63581949e98a8798285268f1b9e4b7d737a
abmetko.a <abmetko.a@gmail.com>
2022-01-16 18:51:46 +0300

    refactoring

---
+++ src/test/java/com/javc/api/MyApiTest.java
@@ -0,0 +1,11 @@
+package com.javc.api;
+
+import org.junit.jupiter.api.Test;
+
+public class MyApiTest {
+
+    @Test
+    public void apiTestShouldBePassed() {
+        System.out.println("test: " + MyApiTest.class.getSimpleName());
+    }
+}
---
+++ src/test/java/com/javc/ui/MyUiTest.java
@@ -0,0 +1,11 @@
+package com.javc.ui;
+
+import org.junit.jupiter.api.Test;
+
+public class MyUiTest {
+
+    @Test
+    public void uiTestShouldBePassed() {
+        System.out.println("test: " + MyUiTest.class.getSimpleName());
+    }
+}

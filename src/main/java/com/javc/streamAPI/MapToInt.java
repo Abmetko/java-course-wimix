5121e7bb38e49e55d7e708d3a585824ca9613df8
abmetko.a <abmetko.a@gmail.com>
2022-01-28 09:39:17 +0300

    refactoring

---
+++ src/main/java/com/javc/streamAPI/MapToInt.java
@@ -0,0 +1,25 @@
+package com.javc.streamAPI;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class MapToInt {
+
+    public static void main(String[] args) {
+
+    }
+
+    private void transformStreamToIntStream() {
+        Stream<Integer> stream = Stream.of(1, 2, 3, 4, 5);
+
+        IntStream intStream = stream.mapToInt(i -> i);
+
+        System.out.println(intStream.sum());
+    }
+
+    private void transformIntStreamToStream() {
+        IntStream intStream = IntStream.rangeClosed(1, 5);
+
+        System.out.println(intStream.boxed());
+    }
+}

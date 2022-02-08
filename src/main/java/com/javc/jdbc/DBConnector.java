62550254268feeefa29668b3056466dfe325d66c
abmetko.a <abmetko.a@gmail.com>
2022-02-08 15:40:27 +0300

    refactored DBConnector.java

--- src/main/java/com/javc/jdbc/DBConnector.java
+++ src/main/java/com/javc/jdbc/DBConnector.java
@@ -7,18 +7,35 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 import java.util.Properties;
 
+import static com.javc.properties.PropertyProvider.getProperty;
+
 public class DBConnector {
 
     private static Statement statement = null;
     private static Connection connection = null;
 
+    /**
+     * Before you can connect to a database, you need to load the driver. There are two methods available, and it depends on your code which is the best one to use.
+     * <p>
+     * In the first method, your code implicitly loads the driver using the Class.forName() method. For PostgreSQL™, you would use:
+     * <p>
+     * Class.forName("org.postgresql.Driver");
+     * This will load the driver, and while loading, the driver will automatically register itself with JDBC.
+     * <p>
+     * This is the most common method to use, but restricts your code to use just PostgreSQL™. If your code may access another database system in the future, and you do not use any PostgreSQL™-specific extensions, then the second method is advisable.
+     * <p>
+     * The second method passes the driver as a parameter to the JVM as it starts, using the -D argument. Example:
+     * <p>
+     * java -Djdbc.drivers=org.postgresql.Driver example.ImageViewer
+     * In this example, the JVM will attempt to load the driver as part of its initialization. Once done, the ImageViewer is started.
+     */
     @SneakyThrows
     public void getConnection() {
         Class.forName("org.postgresql.Driver");
-        String url = "jdbc:postgresql://ec2-52-208-185-143.eu-west-1.compute.amazonaws.com/dbbte6nc0n2o6f";
+        String url = getProperty("jdbc.url");
         Properties props = new Properties();
-        props.setProperty("user", "diralmgxzqsotw");
-        props.setProperty("password", "ca22a15acd926a843f3539a4d859ab99d1a9158c17688e2b8f71f931fc82d9bd");
+        props.setProperty("user", getProperty("jdbc.user"));
+        props.setProperty("password", getProperty("jdbc.password"));
         connection = DriverManager.getConnection(url, props);
     }
 

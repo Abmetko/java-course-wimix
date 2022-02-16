e0d1b454f8a702d02a92f85cfbe13549a25de883
abmetko.a <abmetko.a@gmail.com>
2022-02-16 23:06:43 +0300

    refactoring

--- src/main/java/com/javc/properties/PropertyProvider.java
+++ src/main/java/com/javc/properties/PropertyProvider.java
@@ -9,13 +9,12 @@ import java.util.Properties;
 public class PropertyProvider {
 
     private final static String FILE_PATH = "/application.properties";
-
     private static Properties properties;
 
     private PropertyProvider() {
     }
 
-    public static String getProperty(String propertyName) {
+    public static synchronized String getProperty(String propertyName) {
         if (properties == null) {
             loadProperty();
         }
@@ -27,8 +26,8 @@ public class PropertyProvider {
         }
     }
 
-    private static synchronized void loadProperty() {
-        properties = new Properties();//нужна синхронизация
+    private static void loadProperty() {
+        properties = new Properties();
         try {
             properties.load(PropertyProvider.class.getResourceAsStream(FILE_PATH));///Users/andrey/Desktop/java-course-wimix/target/classes/application.properties
         } catch (IOException e) {
@@ -40,7 +39,7 @@ public class PropertyProvider {
         properties.setProperty(key, value);
     }
 
-    public static void storeProperty(String propName, String propValue) {
+    public static synchronized void storeProperty(String propName, String propValue) {
         if (properties == null) {
             loadProperty();
         }

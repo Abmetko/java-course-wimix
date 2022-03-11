b6530472415374858f16ebffe711e9c3ea5c536e
abmetko.a <abmetko.a@gmail.com>
2022-03-11 21:41:48 +0300

    refactoring

--- src/main/java/com/javc/date_time/DateTimeTest.java
+++ src/main/java/com/javc/date_time/DateTimeTest.java
@@ -1,6 +1,5 @@
 package com.javc.date_time;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -8,33 +7,36 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
 
 public class DateTimeTest {
 
-    public static void main(String[] args) throws ParseException {
-        Date date = new Date();
-
-        System.out.println("Current date & time as human-readable date: " + date);
-        System.out.println("Current date & time as timestamp(epoch): " + date.getTime());//since 1970
-
-        // #1 Извлекли строку с датой и временем из ui элемента с целью проверить на корректность формата данных
-        System.out.println("Valid case: " + validateDateTimeFormat("E MMM dd HH:mm:ss z yyyy", "Mon Dec 27 21:28:32 MSK 2021"));
-        System.out.println("Invalid case: " + validateDateTimeFormat("E MMMM dd HH:mm:ss z yyyy", "Mon Dec 27 21:28:32 MSK 2021"));
-        System.out.println("Invalid case: " + validateDateTimeFormat("E MMM dd HH/mm/ss z yyyy", "Mon Dec 27 21:28:32 MSK 2021"));
-        System.out.println("Invalid case: " + validateDateTimeFormat("E MMM dd HH:M:ss z yyyy", "Mon Dec 27 21:28:32 MSK 2021"));
-        System.out.println("Invalid case: " + validateDateTimeFormat("MMMM dd HH:M:ss z yyyy", "Mon Dec 27 21:28:32 MSK 2021"));
-        System.out.println("Invalid case: " + validateDateTimeFormat("E MMMM dd HH:M:ss yyyy", "Mon Dec 27 21:28:32 MSK 2021"));
-
-        System.out.println("\n");
-        // #2 Извлекли строки ui элементов, и надо проверить, что элементы с датами расположены в правильном порядке на экране
-        System.out.println("Valid sorting case: " + validateSorting(new String[]{"10/15/2021, 11:14", "10/15/2021, 11:15", "10/15/2021, 12:34"}));
-        System.out.println("Invalid sorting case: " + validateSorting(new String[]{"10/15/2021, 11:15", "10/15/2021, 11:14", "10/15/2021, 12:34"}));
-
-        System.out.println("\n");
-        //#3 Проверить, что дата в ui элементе соответствует ожидаемой дате(например, текущая плюс 6 месяцев)
-        System.out.println("Is actual date & time from UI and expected are equals: " + validateFutureDate("02 August 2022"));
-    }
+//    public static void main(String[] args) throws ParseException {
+//        Date date = new Date();
+//
+//        System.out.println("Current date & time as human-readable date: " + date);
+//        System.out.println("Current date & time as timestamp(epoch): " + date.getTime());//since 1970
+//
+//        // #1 Извлекли строку с датой и временем из ui элемента с целью проверить на корректность формата данных
+//        System.out.println("Valid case: " + validateDateTimeFormat("E MMM dd HH:mm:ss z yyyy", "Mon Dec 27 21:28:32 MSK 2021"));
+//        System.out.println("Invalid case: " + validateDateTimeFormat("E MMMM dd HH:mm:ss z yyyy", "Mon Dec 27 21:28:32 MSK 2021"));
+//        System.out.println("Invalid case: " + validateDateTimeFormat("E MMM dd HH/mm/ss z yyyy", "Mon Dec 27 21:28:32 MSK 2021"));
+//        System.out.println("Invalid case: " + validateDateTimeFormat("E MMM dd HH:M:ss z yyyy", "Mon Dec 27 21:28:32 MSK 2021"));
+//        System.out.println("Invalid case: " + validateDateTimeFormat("MMMM dd HH:M:ss z yyyy", "Mon Dec 27 21:28:32 MSK 2021"));
+//        System.out.println("Invalid case: " + validateDateTimeFormat("E MMMM dd HH:M:ss yyyy", "Mon Dec 27 21:28:32 MSK 2021"));
+//
+//        System.out.println("\n");
+//        // #2 Извлекли строки ui элементов, и надо проверить, что элементы с датами расположены в правильном порядке на экране
+//        System.out.println("Valid sorting case: " + validateSorting(new String[]{"10/15/2021, 11:14", "10/15/2021, 11:15", "10/15/2021, 12:34"}));
+//        System.out.println("Invalid sorting case: " + validateSorting(new String[]{"10/15/2021, 11:15", "10/15/2021, 11:14", "10/15/2021, 12:34"}));
+//
+//        System.out.println("\n");
+//        //#3 Проверить, что дата в ui элементе соответствует ожидаемой дате(например, текущая плюс 6 месяцев)
+//        System.out.println("Is actual date & time from UI and expected are equals: " + validateFutureDate("02 August 2022"));
+//    }
 
     /*
      dd-M-yyyy hh:mm:ss	        02-1-2018 06:07:59
@@ -98,5 +100,17 @@ public class DateTimeTest {
     public static void testSimpleDateFormat() {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z");
         System.out.println(simpleDateFormat.format(new Date()));
+    }
+
+    //2022-04-04T00:00:00.000+05:30
+    public static void testZonedDateTime2() {
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+
+        ZonedDateTime zonedDateTimeLocal = ZonedDateTime.ofInstant(Instant.now(), ZoneId.systemDefault());
+        System.out.println(zonedDateTimeLocal.format(dateTimeFormatter));
+    }
+
+    public static void main(String[] args) {
+        testZonedDateTime2();
     }
 }

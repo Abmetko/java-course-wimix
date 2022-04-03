f80137d27a06a1c6697d8d5b3826802adc309d32
abmetko.a <abmetko.a@gmail.com>
2022-04-03 23:12:58 +0300

    refactored com.javc.program_arguments.PassingProgramArguments

--- src/main/java/com/javc/program_arguments/PassingProgramArguments.java
+++ src/main/java/com/javc/program_arguments/PassingProgramArguments.java
@@ -5,10 +5,12 @@ import java.util.stream.Stream;
 public class PassingProgramArguments {
 
     public static void main(String[] args) {
-        Stream.of(args).forEach(System.out::println); //Program arguments: arg1 arg2 arg3
+        Stream.of(args).forEach(System.out::println);
 
-        System.out.println(System.getProperty("javc.env")); //VM options: -Djavc.env=develop
-
-        //Running with command line: java -Djavc.env=develop PassingProgramArguments.java arg1 arg2 arg3
+        /*
+        Running with command line:
+        cd /Users/andrey/Desktop/java-course-wimix/src/main/java/com/javc/program_arguments/
+        java PassingProgramArguments.java 100 200 300
+         */
     }
 }

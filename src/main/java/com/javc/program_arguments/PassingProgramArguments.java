1af95f3a6bb2e113950cbaedcef618ad173f2afa
abmetko.a <abmetko.a@gmail.com>
2022-04-05 22:28:51 +0300

    refactoring

--- src/main/java/com/javc/program_arguments/PassingProgramArguments.java
+++ src/main/java/com/javc/program_arguments/PassingProgramArguments.java
@@ -4,13 +4,12 @@ import java.util.stream.Stream;
 
 public class PassingProgramArguments {
 
-    public static void main(String[] args) {
-        Stream.of(args).forEach(System.out::println);
-
-        /*
+    /*
         Running with command line:
         cd /Users/andrey/Desktop/java-course-wimix/src/main/java/com/javc/program_arguments/
         java PassingProgramArguments.java 100 200 300
          */
+    public static void main(String[] args) {
+        Stream.of(args).forEach(System.out::println);
     }
 }
--- src/test/java/com/javc/api/BaseTest.java
+++ src/test/java/com/javc/api/BaseTest.java
@@ -6,6 +6,13 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.TestInstance;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+/*
+@Execution(ExecutionMode.CONCURRENT) it's the same as junit.jupiter.execution.parallel.mode.default=concurrent
+- for running test methods in class with concurrent mode.
+@ResourceLock("BlaBla") is used to declare that the annotated test class or test method requires access to
+a shared resource identified by a key.
+*/
+
 public abstract class BaseTest {
 
     protected ClimateService climateService;
